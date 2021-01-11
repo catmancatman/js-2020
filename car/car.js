df165d8665377ad8658765230bcda633fd608440
JavaScript 2020/21 <javascript202021@iMac-Admin-2.local>
2021-01-11 20:51:58 +0200

    curs 11 ianuarie 2021

--- car/car.js
+++ car/car.js
@@ -5,8 +5,28 @@ let dacia = {
     year: 1978,
     mileage: 525737,
     maxSpeed: 180,
-    paint: function(color) {
+    averageConsumption: 11.5,
+    batteryCapacity: 150,
+    batteryCharge: 150,
+    paint(color) {
         this.color = color;
+    },
+    move: function name(distance) {
+        consumption = this.averageConsumption * distance / 100
+        if (consumption > this.batteryCharge){
+            this.batteryCharge = 0
+            this.mileage += this.distanceToEmpty()
+    
+        } else{
+        this.batteryCharge -= consumption
+        this.mileage += distance
+        }
+    },
+    recharge() {
+        this.batteryCharge = this.batteryCapacity
+    },
+    distanceToEmpty() {
+        return 100 * this.batteryCharge / this.averageConsumption
     }
 }
 
@@ -23,6 +43,9 @@ let ferrari = {
     }  
 }
 dacia.paint("galben")
+dacia.move(100)
+console.log(Math.round(dacia.distanceToEmpty()))
+dacia.move(100)
+console.log(dacia.distanceToEmpty())
 console.log(dacia)
 console.log(ferrari)
-document.g
