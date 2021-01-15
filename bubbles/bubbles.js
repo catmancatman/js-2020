738b9c6ffb10763f4e9b06ace19803fa56994dc1
JavaScript 2020/21 <javascript202021@iMac-Admin-2.local>
2021-01-15 20:50:36 +0200

    curs 5

---
+++ bubbles/bubbles.js
@@ -0,0 +1,17 @@
+var numberOfBubbles = 50
+for (let i = 0; i < numberOfBubbles; i++) {
+    newBubble() 
+}
+function newBubble() {
+    let bubble = document.createElement("div")
+    bubble.classList.add("bubble")
+    let x = randomNumber(100)
+    let delay = randomNumber(5000)
+    bubble.style.left = x + "vw"
+    bubble.style.animationDelay =delay + "ms"
+    document.querySelectorAll("body").appendChild(bubble)
+}
+function randomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
