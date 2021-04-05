adc2680235413df4d6f2c4c8a456924391451572
catalin <catmanpetreseniuc@yahoo.co.uk>
2021-04-05 20:52:37 +0300

    5 4 2021 web dev

---
+++ webdev/classes.js
@@ -0,0 +1,57 @@
+class Om {
+
+}
+class Animal {
+    constructor(age) {
+        this.age = age;
+    }
+
+}
+class Dog {
+    cry (){
+        console.log
+    }
+}
+//Clasa
+// Relatia  ia-a
+class Cat extendes Animal{
+
+    //constructor este folosit pentru atribute
+    constructor(age){
+        //
+        super(age) = age;
+        //Has a relation
+        this.owner = owner
+
+    }
+    //metoda
+    scry () {
+        console.log("Miau");
+    }
+}
+
+    //atribut
+    var age = 2;
+    //metoda
+    fuction cry () {
+        console.log("Miau");
+    }
+}
+
+class RussianBlueCat extends Cat {
+
+}
+
+//crearea altui obiect
+let pisicaRino = new RussianBlueCat(2);
+
+let pisicaTommy = new Cat(2);
+let pisicaTommy2 = new Cat(2);
+let pisicaTommy3 = new Cat(2);
+
+
+verificaSunetul (pisicaRino);
+verificaSunetul 
+//chem o fuctie din clasa
+pisicaRino.cry();
+pisicaTommy.cry();
