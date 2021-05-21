1011d0008bf29e72c8eb6b7842d56c5b396ea2ea
catalin <catmanpetreseniuc@yahoo.co.uk>
2021-05-21 21:03:58 +0300

    21 mai 2021

--- vue/project/hanoi/src/App.vue
+++ vue/project/hanoi/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <hanoi></hanoi>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Hanoi from './components/Hanoi.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  Hanoi
   }
 }
 </script>
